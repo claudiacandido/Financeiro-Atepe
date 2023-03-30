bebf7f11306f2ab4250c8a6e375cf175960bd2fc
claudiacandido <claudiafmcandido@gmail.com>
2023-03-29 22:02:35 -0300

    Delete src/main/java/org/atepe directory

--- src/main/java/org/atepe/repository/DespesaRepository.java
+++
@@ -1,9 +0,0 @@
-package org.atepe.repository;
-
-import org.atepe.model.Despesa;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DespesaRepository extends CrudRepository<Despesa, Integer> {
-	Despesa findByCodDespesa(Integer codDespesa);
-
-}
--- src/main/java/org/atepe/repository/PagamentoRepository.java
+++
@@ -1,9 +0,0 @@
-package org.atepe.repository;
-
-import org.atepe.model.Pagamento;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PagamentoRepository extends CrudRepository<Pagamento, Integer> {
-	Pagamento findByCodPagamento(Integer codPagamento);
-
-}
